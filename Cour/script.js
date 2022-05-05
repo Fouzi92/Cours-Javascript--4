7083572ee8f725be0f0dbf7cbb96efb58e1d3a97
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-05 13:05:12 +0200

    suite

--- Cour/script.js
+++ Cour/script.js
@@ -21,10 +21,42 @@ req.send();
 //})
 //.catch((err) => console.log(err)); //CATCH = si le Then ne fonction pas envoi-moi une erreur
 
-fetch("data.txt")
-  .then((res) => res.text())
-  .then((data) => console.log(data));
+//fetch("data.txt")
+//.then((res) => res.text())
+//.then((data) => console.log(data));
 
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+fetch("data.json").then((res) => res.json());
+//.then((data) => console.log(data));
+
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+// fetch("data.json", init).then((res) => console.log(res));
+
+// CRUD => Create (POST), read (GET) , Update (PUT),  delete (DELETE)
+
+const init2 = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // La méthode JSON.stringify() convertit une valeur JavaScript en chaîne JSON. Optionnellement, elle peut remplacer des valeurs ou spécifier les propriétés à inclure si un tableau de propriétés a été fourni.
+  body: JSON.stringify({
+    pseudo: "Chomeur",
+    message: "Salut les chomeurs",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/posts", init2).then(() =>
+    console.log("data envoyée")
+  );
+});
