130ea4a30f69fb96b18c8061c542c170dcfa1add
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-05 17:50:34 +0200

    suite

--- user-app/script-2.js
+++ user-app/script-2.js
@@ -13,8 +13,30 @@ const fetchUser = async () => {
 const userDisplay = async () => {
   await fetchUser();
 
-  document.body.innerHTML = userData.map(
-    (user) => `<h3>${user.name.first}</h3>`
-  );
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return newDate;
+  };
+
+  const dayCalc = (date) => {
+    let today = new Date();
+
+    return today;
+  };
+
+  document.body.innerHTML = userData
+    .map(
+      (user) => `<div class="card">
+      <img src=${user.picture.large} alt="photo de ${user.name.last}">
+    <h3>${user.name.first}</h3>
+    <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
+    <em>Menbre depuis : ${dayCalc(user.registered.date)} jours </em>
+    </div>`
+    )
+    .join("");
 };
 userDisplay();
