0353abbe66148062204bb2dcebd5a60e7f63e1d5
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-06 17:57:46 +0200

    suite

--- Projet - Cooking App/script-3.js
+++ Projet - Cooking App/script-3.js
@@ -1,11 +1,38 @@
 // https://www.themealdb.com/api/json/v1/1/search.php?s=tomato
+const result = document.getElementById("result");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
 let meals = [];
 
-const fetchMeals = async () => {
-  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=duck")
+const fetchMeals = async (search) => {
+  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + search)
     .then((res) => res.json())
     .then((data) => (meals = data.meals));
 
   console.log(meals);
 };
-fetchMeals();
+
+function mealsDisplay() {
+  meals.length = 12;
+
+  result.innerHTML = meals
+    .map(
+      (meal) =>
+        `  <li class="card">
+             <h2>${meal.strMeal}</h2>
+             <p>${meal.strArea}</p>
+             <img src=${meal.strMealThumb} alt="photo ${meal.strMeal}">
+             <ul></ul>
+        </li>
+        `
+    )
+    .join("");
+}
+input.addEventListener("input", (e) => {
+  fetchMeals(e.target.value); //.then(() => mealsDisplay());//pour avoir des resulta en tapent sur la bar de recherche
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  mealsDisplay();
+});
